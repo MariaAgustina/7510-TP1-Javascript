d69061b0aa293624e18ef29185dfd04aff51d6cf
agustina markosich <agustina.markosich@mercadolibre.com>
2017-10-07 00:09:47 -0300

    facts tests passing

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,14 +1,59 @@
-//add comment to test commit 
+//Sentence could be a fact or a rule
 var Interpreter = function () {
+	this.factsDictionary = {};
+	this.rulesDictionary = {};
 
-    this.parseDB = function (params, paramss, paramsss) {
+	this.isAFact = function(sentence){
+		return (sentence.indexOf(":-") === -1);
+	}
 
+	this.getKeyForSentence = function(sentence){
+		return sentence.substr(0, sentence.indexOf('(')); 
+	}
+
+    this.parseDB = function (database) {
+
+    	this.addSentenceToDictionary = function(sentence,setenceDictionary){
+    		var key = this.getKeyForSentence(sentence);
+    		if(setenceDictionary[key]){
+    			setenceDictionary[key].push(sentence);
+    		}else{
+    			setenceDictionary[key] = [sentence];
+    		}
+    	}
+
+    	for (position = 0; position < database.length; position++) {
+			var sentence =database[position];
+
+			if(this.isAFact(sentence)){
+				this.addSentenceToDictionary(sentence,this.factsDictionary);
+			}else{
+				this.addSentenceToDictionary(sentence,this.rulesDictionary);
+			}
+		}
     }
 
-    this.checkQuery = function (params) {
-        return true;
+    this.checkFact = function(fact){
+    	//console.log(this.factsDictionary);
+		//console.log(this.rulesDictionary);
+    	var key = this.getKeyForSentence(fact);
+    	var factsArray = this.factsDictionary[key];
+    	console.log(key);
+    	console.log(factsArray);
+    	console.log(fact);
+    	console.log(factsArray.includes(fact));
+    	return factsArray.includes(fact);
+    }
+
+    this.checkQuery = function (query) {
+        if(this.isAFact(query)){
+        	return this.checkFact(query + ".");
+        }else{
+        	return false;
+        }
     }
 
 }
+
 
 module.exports = Interpreter;
